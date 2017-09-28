e07890ca6941327b1a5c9d4bcb018affbdc40af2
VickiLi2017 <vl54@njit.edu>
2017-09-28 13:43:18 -0400

    Project initial load to repository

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,37 @@
+<!-- create.blade.php -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>My Laravel 5.5 CRUD Tutorial With Example From Scratch </title>
+    		<link rel="stylesheet" href="{{asset('css/app.css')}}">
+	</head>
+	<body>
+		<div class="container">
+			<h2> Create A New Product</h2><br />
+			<form>
+				<div class="row">
+          				<div class="col-md-4"></div>
+			        	<div class="form-group col-md-4">
+				        	<label for="name">Name:</label>
+				        	<input type="text" class="form-control" name="name">
+				        </div>
+			        </div>
+        			<div class="row">
+				        <div class="col-md-4">
+		        		<div class="form-group col-md-4">
+				              <label for="price">Price:</label>
+				              <input type="text" class="form-control" name="price">
+				        </div>
+				        </div>
+        			</div>
+			        <div class="row">
+				         <div class="col-md-4">
+				         <div class="form-group col-md-4">
+            					<button type="submit" class="btn btn-success" style="margin-left:38px">Add Product</button>
+			         </div>
+				 </div>
+			</form>
+		</div>
+	</body>
+</html>
